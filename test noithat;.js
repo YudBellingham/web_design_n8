8eb2b455138dd1224acb38ae45a11e1fd6095022
YudBellingham <quanduysoma1@gmail.com>
2024-12-25 07:51:57 +0700

    Add files via upload

---
+++ test noithat;.js
@@ -0,0 +1,11 @@
+let cart = [];
+
+function addToCart(product) {
+    cart.push(product);
+    alert(`${product} đã được thêm vào giỏ hàng!`);
+    console.log(cart); // Debug để kiểm tra danh sách giỏ hàng
+}
+
+
+
+
